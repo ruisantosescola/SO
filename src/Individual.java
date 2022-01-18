1f2da4701817d910404d900b8541552d7e2c0bd0
Rui Santos <202000872@estudantes.ips.pt>
2022-01-18 20:04:00 +0000

    calculoCortes

--- src/Individual.java
+++ src/Individual.java
@@ -1,40 +1,93 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public class Individual {
     private int arr[];
-    private int numTotalDeCortes; //number of diferente lenght
+    private int cumprimentoTotaldePecas;
     private int size;
     private int numberOfCuts;
     private Params params;
+    private List<Integer> listaDeCortes;
+    private List<Integer> listaDeDesperdicios;
+    private List<Integer> listaDeItems;
 
 
 
-    public Individual() {
+    public Individual(Params params) {
+        this.params = params;
+        this.listaDeCortes = new ArrayList<>();
+        this.listaDeDesperdicios = new ArrayList<>();
+        this.listaDeItems = new ArrayList<>();
+        this.cumprimentoTotaldePecas = 0;
 
     }
 
 
 
-    public ArrayList<Integer> listaDeItens(Params params){
-        ArrayList<Integer> listaFinal = new ArrayList<>();
-
+    public void listaDeItens(){
         for(int i = 0; i < params.getM(); i++){
+            cumprimentoTotaldePecas += params.getQttPecas()[i];
             for(int j = 0; j < params.getQttPecas()[i]; j++){
-                listaFinal.add(params.getCompPecas()[i]);
+                listaDeItems.add(params.getCompPecas()[i]);
             }
         }
-
-        Collections.shuffle(listaFinal);
-
-        return listaFinal;
+        Collections.shuffle(listaDeItems);
     }
 
 
+    public void calculoCortes(){
+        int sum = 0;
+        int count = 0;
+        for(int j = 0; j < cumprimentoTotaldePecas; j++){
+            sum += listaDeItems.get(j);
+            if((sum >= params.getMaxComprimento()) || (j == cumprimentoTotaldePecas-1)){
+                for(int i = 0; i < count; i++){
+                    if(i == count - 1){
+                        if(j == cumprimentoTotaldePecas-1){
+                            listaDeCortes.add(0);
+                        }
+                        listaDeCortes.add(1);
+                        if(sum == params.getMaxComprimento()){
+                            listaDeDesperdicios.add(params.getMaxComprimento() - sum );
+                            listaDeCortes.remove(j - 1);
+                            listaDeCortes.add(0);
+                            listaDeCortes.add(1);
+                            j++;
+                        }else{
+                            if(j == cumprimentoTotaldePecas-1){
+                                listaDeDesperdicios.add(params.getMaxComprimento() - sum );
+                            }else{
+                                listaDeDesperdicios.add(params.getMaxComprimento() - (sum - listaDeItems.get(j)));
+                            }
+
+                        }
+                        sum = 0;
+                        count = -1;
+                        j--;
+                    }else{
+                        listaDeCortes.add(0);
+                    }
+                }
+            }
+            count++;
+        }
+    }
 
 
+    public Params getParams() {
+        return params;
+    }
 
+    public List<Integer> getListaDeCortes() {
+        return listaDeCortes;
+    }
 
+    public List<Integer> getListaDeDesperdicios() {
+        return listaDeDesperdicios;
+    }
 
-
+    public List<Integer> getListaDeItems() {
+        return listaDeItems;
+    }
 }
--- src/main.java
+++ src/main.java
@@ -5,10 +5,16 @@ public class main {
 
     public static void main(String[] args) {
         Params params = Params.readfile("src/pcu_tests_tp2/prob03.txt");
-        Individual individual = new Individual();
+        Individual individual = new Individual(params);
+
+        individual.listaDeItens();
+        individual.calculoCortes();
+
+        System.out.println(individual.getListaDeItems());
+        System.out.println(individual.getListaDeCortes());
+        System.out.println(individual.getListaDeDesperdicios());
 
 
-        System.out.println(individual.listaDeItens(params));
 
     }
 
