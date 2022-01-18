8447b7db06e970292d7e04b1b92240849a0e46ac
Rui Santos <202000872@estudantes.ips.pt>
2022-01-18 11:50:53 +0000

    test

--- src/Individual.java
+++ src/Individual.java
@@ -1,6 +1,11 @@
 
 
 public class Individual {
+    private int arr[];
+    private int a; //number of diferente lenght
+    private int size;
+    private int numberOfCuts;
+
 
 
     public Individual() {
