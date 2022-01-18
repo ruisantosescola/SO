b9e2965f86d52a812da9f3e789155fcfa2547c6d
Rui Santos <202000872@estudantes.ips.pt>
2022-01-18 18:40:04 +0000

    listaDeItens

--- src/Individual.java
+++ src/Individual.java
@@ -1,16 +1,38 @@
-
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Individual {
     private int arr[];
-    private int a; //number of diferente lenght
+    private int numTotalDeCortes; //number of diferente lenght
     private int size;
     private int numberOfCuts;
+    private Params params;
 
 
 
     public Individual() {
 
     }
+
+
+
+    public ArrayList<Integer> listaDeItens(Params params){
+        ArrayList<Integer> listaFinal = new ArrayList<>();
+
+        for(int i = 0; i < params.getM(); i++){
+            for(int j = 0; j < params.getQttPecas()[i]; j++){
+                listaFinal.add(params.getCompPecas()[i]);
+            }
+        }
+
+        Collections.shuffle(listaFinal);
+
+        return listaFinal;
+    }
+
+
+
+
 
 
 
--- src/main.java
+++ src/main.java
@@ -1,10 +1,18 @@
-import static javafx.application.Application.launch;
+
 
 public class main {
 
+
     public static void main(String[] args) {
-        launch(args);
+        Params params = Params.readfile("src/pcu_tests_tp2/prob03.txt");
+        Individual individual = new Individual();
+
+
+        System.out.println(individual.listaDeItens(params));
+
     }
+
+
 
 
 
